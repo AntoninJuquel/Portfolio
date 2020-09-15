efcf46716f8893eba81cab075542dcb1ec5e7375
AntoninJuquel <57025128+AntoninJuquel@users.noreply.github.com>
2020-09-15 20:14:03 +0200

    Changed path

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
   const content = {
     title: 'Antonin JUQUEL',
     pages: [
-      { id: 1, title: 'Home', path: '/home', render: <HomePage title='Mon portfolio' subTitle='Développeur web et applications' catchPhrase='Prenez connaissance des mes projets ci-dessous' /> },
-      { id: 2, title: 'About', path: '/about', render: <AboutPage title='A propos de moi' /> },
-      { id: 3, title: 'Contact', path: '/contact', render: <ContactPage title='Me contacter' /> },
+      { id: 1, title: 'Home', path: '/Portfolio/home', render: <HomePage title='Mon portfolio' subTitle='Développeur web et applications' catchPhrase='Prenez connaissance des mes projets ci-dessous' /> },
+      { id: 2, title: 'About', path: '/Portfolio/about', render: <AboutPage title='A propos de moi' /> },
+      { id: 3, title: 'Contact', path: '/Portfolio/contact', render: <ContactPage title='Me contacter' /> },
     ]
   }
 
@@ -49,10 +49,6 @@ function App() {
         </Navbar>
 
         <Route exact path='/' render={() => (
-          <Redirect to='/home' />
-        )} />
-
-        <Route exact path='/Portfolio/' render={() => (
           <Redirect to='/home' />
         )} />
 
