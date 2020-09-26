130e41f8f8afa2cf9d390356850390354e5b24a0
antonin.juquel@outlook.fr <57025128+AntoninJuquel@users.noreply.github.com>
2020-09-26 13:21:55 +0200

    Create Internships.js

---
+++ src/utils/Internships.js
@@ -0,0 +1,12 @@
+const InternShips =
+    [
+        {
+            id: 0,
+            title: 'Uballers',
+            subTitle: 'Start-up',
+            imgSrc: 'https://uballers.com/assets/img/brand/BallonRouge.png',
+            link: 'https://antoninjuquel.github.io/test-for-Uballers/',
+            selected: false,
+        }
+    ]
+export default InternShips
