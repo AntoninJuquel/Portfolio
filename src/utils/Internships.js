69f81e7b4e23362197f6f3fea81578474f835ede
AntoninJuquel <57025128+AntoninJuquel@users.noreply.github.com>
2020-09-27 01:16:07 +0200

    Added Uballers skills

--- src/utils/Internships.js
+++ src/utils/Internships.js
@@ -1,3 +1,6 @@
+import React from "react";
+import Hero from "../components/Hero"
+
 const InternShips =
     [
         {
@@ -5,8 +8,31 @@ const InternShips =
             title: 'Uballers',
             subTitle: 'Start-up',
             imgSrc: 'https://uballers.com/assets/img/brand/BallonRouge.png',
-            link: 'https://antoninjuquel.github.io/test-for-Uballers/',
-            selected: false,
+            link: 'https://uballers.com/home',
+            description:
+                [
+                    <Hero key='0' title='Uballers' sentence='Uballers est une start up dont le but est de monter un réseau social de basketeurs.' />,
+                    <div key='1'>
+                        <h3>Mon rôle dans ce stage a été de mettre en place son application mobile à l'image de son <a href='https://uballers.com/home' rel="noopener noreferrer" target='_blank'>site internet</a></h3>
+                        <p>A1.2.1 Élaboration et présentation d'un dossier de choix de solution</p>
+                        <p>A1.3.3 Accompagnement de la mise en place d'un nouveau service</p>
+                        <p>A1.3.4 Déploiement d'un service</p>
+                        <p>A1.4.1 Participation à un projet</p>
+                        <p>A2.2.1 Suivi et résolution d'incidents</p>
+                        <p>A2.3.1 Identification, qualification et évaluation d'un problème</p>
+                        <p>A4.1.1 Proposition d'une solution applicative</p>
+                        <p>A4.1.2 Conception ou adaptation de l'interface utilisateur d'une solution</p>
+                        <p>A4.1.8 Réalisation des tests nécessaires à la validation d'éléments</p>
+                        <p>A4.1.9 Rédaction d'une documentation technique</p>
+                        <p>A4.1.1 Rédaction d'une documentation d'utilisation</p>
+                        <p>A4.2.1 Analyse et correction d'un dysfonctionnement, d'un problème</p>
+                        <p>A4.2.3 Réalisation des tests nécessaires à la mise en production</p>
+                        <p>A4.2.4 Mise à jour d'une documentation technique</p>
+                        <p>A5.1.6 Évaluation d'un investissement informatique</p>
+                        <p>A5.2.3 Repérage des compléments de formation ou d'auto-formation</p>
+                        <p>A5.2.4 Étude d‘une technologie, d'un composant, d'un outi</p>
+                    </div>
+                ]
         }
     ]
 export default InternShips
