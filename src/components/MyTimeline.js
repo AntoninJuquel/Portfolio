bb4579d9fee02ffcc1e798d14f9d1f8fc43df860
AntoninJuquel <57025128+AntoninJuquel@users.noreply.github.com>
2020-10-02 18:54:35 +0200

    Update MyTimeline.js

--- src/components/MyTimeline.js
+++ src/components/MyTimeline.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import { Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent, TimelineDot, TimelineOppositeContent } from "@material-ui/lab";
 import Typography from '@material-ui/core/Typography';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        padding: '6px 16px',
-    },
-}));
-
-function MyTimeline({ align }) {
-    const classes = useStyles();
+function MyTimeline({ align, items }) {
     return (
         <Timeline align={align}>
 
-            <TimelineItem>
-                <TimelineOppositeContent>
-                    <Typography variant="body2" color="textSecondary">9:30 am</Typography>
-                </TimelineOppositeContent>
-                <TimelineSeparator>
-                    <TimelineDot color="primary" variant="outlined">
-                        <FastfoodIcon />
-                    </TimelineDot>
-                    <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>
-                    <Paper elevation={3} className={classes.paper}>
-                        <Typography variant="h6" component="h1">Eat</Typography>
-                        <Typography>Because you need strength</Typography>
-                    </Paper>
-                </TimelineContent>
-            </TimelineItem>
+            {
+                items &&
+                items.map(el => {
+                    return (
+                        <TimelineItem>
+                            <TimelineOppositeContent>
+                                <Typography variant="body2" color={el.dateColor}>{el.date}</Typography>
+                            </TimelineOppositeContent>
+                            <TimelineSeparator>
+                                <TimelineDot color={el.iconColor} variant="outlined">
+                                    {el.icon}
+                                </TimelineDot>
+                                <TimelineConnector />
+                            </TimelineSeparator>
+                            <TimelineContent>
+                                <Paper elevation={3} style={{textAlign: "center"}}>
+                                    <Typography variant="h6" component="h1" color={el.titleColor}>{el.title}</Typography>
+                                    <Typography color={el.descriptionColor}>{el.description}</Typography>
+                                </Paper>
+                            </TimelineContent>
+                        </TimelineItem>
+                    )
+                })
+            }
 
         </Timeline>
     )
