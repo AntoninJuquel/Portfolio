5ff839e4d8ae74015cffdc70d6f29a78640bcfc6
AntoninJuquel <57025128+AntoninJuquel@users.noreply.github.com>
2020-10-02 18:55:10 +0200

    Updated monitoring components

--- src/components/MonitoringRender.js
+++ src/components/MonitoringRender.js
@@ -1,44 +1,29 @@
+import { Box } from "@material-ui/core";
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import Hero from "./Hero";
+import MyTimeline from "./MyTimeline";
 
-function MonitoringRender({presentation, definition,timeline}) {
-    return(
-        <div style={{ backgroundColor: "#ddd" }}>
-                    <div className="my-center">
-                        <Hero subTitle="La simulation informatique 3D" sentence="Focus sur la technologie du Ray Tracing" />
-                        <Row>
-                            <Col>
-                                <p>Présentation :</p>
-                                <p>{presentation}</p>
-                            </Col>
-                            <Col>
-                                <p>Définition :</p>
-                                <p>{definition}</p>
-                            </Col>
+function MonitoringRender({ presentation, definition, timeline }) {
+    return (
+        <Box style={{ backgroundColor: "#ddd" }}>
+            <Box textAlign="center">
+                <Hero subTitle="La simulation informatique 3D" sentence="Focus sur la technologie du Ray Tracing" />
+                <Row>
+                    <Col>
+                        <p>Présentation :</p>
+                        <p>{presentation}</p>
+                    </Col>
+                    <Col>
+                        <p>Définition :</p>
+                        <p>{definition}</p>
+                    </Col>
+                </Row>
 
-                        </Row>
-                    </div>
-                    <VerticalTimeline>
-                        {
-                           timeline.map(el => {
-                                    return (
-                                        <VerticalTimelineElement
-                                            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
-                                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
-                                            date={el.date}
-                                            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                                        >
-                                            <h3>{el.title}</h3>
-                                            <h4>{el.subTitle}</h4>
-                                            <p>{el.description}</p>
-                                        </VerticalTimelineElement>
-                                    )
-                                })}
+                <MyTimeline align="alternate" items={timeline} />
+            </Box>
 
-                    </VerticalTimeline>
-                </div>
+        </Box>
     )
 }
 
--- src/utils/Monitorings.js
+++ src/utils/Monitorings.js
@@ -12,20 +12,36 @@ export const Monitorings =
             render:
                 <MonitoringRender
                     presentation="J'ai choisi ce sujet de veille technologique car je trouve intéressant de savoir comment fonctionne les différentes techniques de rendu 3D. Parmi ces techniques, le ray tracing permet de rendre les scènes plus réalistes au niveau de l'éclairage. Cette technique utilisée pour la première fois en 1968 [voir infos], est depuis utilisée majoritairement dans le domaine du cinéma."
-                    definition="Le ray tracing est fondamentalement une nouvelle manière de travailler la lumière dans nos jeux vidéo (mais pas que). Ce « traçage de rayon » est en fait le nom donné à une technologie de rendu de la lumière dans une image de synthèse"
+                    definition="Le ray tracing est une technique de rendu graphique qui simule le parcours des rayons lumineux et leur interaction avec divers matériaux."
                     timeline={
                         [
                             {
-                                title: "Ray tracing",
-                                subTitle: "C STYLé",
-                                description: "HOP HOP ",
-                                date: "2020"
+                                title: "Bond en avant du Ray Tracing",
+                                titleColor: "primary",
+                                date: "2018",
+                                descriptionColor: "textPrimary",
+                                description: "Une grande avancée est arrivée pour le ray tracing : la possibilité de le calculer en temps réel dans un environnement 3D. Nvidia présente un GPU capable de faire du ray tracing en temps réel, Turing est donc une architecture conçue avant tout pour la visualisation",
                             },
                             {
-                                title: "",
-                                subTitle: "",
-                                description: "",
-                                date: ""
+                                title: "Monsters University",
+                                titleColor: "primary",
+                                date: "2013",
+                                descriptionColor: "textPrimary",
+                                description: "Premier dessin animé à utiliser le Ray Tracing pour tous ses éffets lumineux et d'ombres",
+                            },
+                            {
+                                title: "Compleat Angler",
+                                titleColor: "primary",
+                                date: "1979",
+                                descriptionColor: "textPrimary",
+                                description: "Premier film à utiliser le Ray Tracing, produit par Turner Whitted ingénieur des Bell Labs",
+                            },
+                            {
+                                title: "Arthur Appel",
+                                titleColor: "primary",
+                                date: "1968",
+                                descriptionColor: "textPrimary",
+                                description: "Ingénieur chez IBM, il en écrivait déjà le premier algorithme",
                             },
                         ]
                     }
