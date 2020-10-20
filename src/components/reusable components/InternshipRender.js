63c82cf4c056f36e101650f8dfdf375e5257f0bd
AntoninJuquel <57025128+AntoninJuquel@users.noreply.github.com>
2020-10-20 15:32:53 +0200

    updated internships

--- src/components/reusable components/InternshipRender.js
+++ src/components/reusable components/InternshipRender.js
@@ -1,18 +1,19 @@
+import { Box } from "@material-ui/core";
 import React from "react";
 import Hero from "./Hero";
 
 function InternshipRender({ title, sentence, introduction, skills }) {
     return (
-        <div className="my-center">
+        <Box>
             <Hero title={title} sentence={sentence} />
 
             <h3>{introduction}</h3>
             {skills.map(skill => {
                 return (
-                    <p>{skill}</p>
+                    <p>{skill.title}</p>
                 )
             })}
-        </div>
+        </Box>
     )
 }
 
--- src/utils/Internships.js
+++ src/utils/Internships.js
@@ -9,26 +9,97 @@ const InternShips =
             subTitle: 'Start-up',
             imgSrc: 'https://uballers.com/assets/img/brand/BallonRouge.png',
             link: 'https://uballers.com/home',
-            render: <InternshipRender title="Uballers" introduction="Mon rôle a été de réaliser une application mobile à l'image de leur site internet" sentence="Uballers est une start up dont le but est de monter un réseau social de basketeurs."
+            render: <InternshipRender
+                title="Uballers"
+                introduction="Mon rôle a été de réaliser une application mobile à l'image de leur site internet"
+                sentence="Uballers est une start up dont le but est de monter un réseau social de basketeurs."
                 skills={
                     [
-                        "A1.2.1 Élaboration et présentation d'un dossier de choix de solution",
-                        "A1.3.3 Accompagnement de la mise en place d'un nouveau service",
-                        "A1.3.4 Déploiement d'un service",
-                        "A1.4.1 Participation à un projet",
-                        "A2.2.1 Suivi et résolution d'incidents",
-                        "A2.3.1 Identification, qualification et évaluation d'un problème",
-                        "A4.1.1 Proposition d'une solution applicative",
-                        "A4.1.2 Conception ou adaptation de l'interface utilisateur d'une solution",
-                        "A4.1.8 Réalisation des tests nécessaires à la validation d'éléments",
-                        "A4.1.9 Rédaction d'une documentation technique",
-                        "A4.1.1 Rédaction d'une documentation d'utilisation",
-                        "A4.2.1 Analyse et correction d'un dysfonctionnement, d'un problème",
-                        "A4.2.3 Réalisation des tests nécessaires à la mise en production",
-                        "A4.2.4 Mise à jour d'une documentation technique",
-                        "A5.1.6 Évaluation d'un investissement informatique",
-                        "A5.2.3 Repérage des compléments de formation ou d'auto-formation",
-                        "A5.2.4 Étude d‘une technologie, d'un composant, d'un outi",
+                        {
+                            title: "A1.2.1 Élaboration et présentation d'un dossier de choix de solution",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A1.3.3 Accompagnement de la mise en place d'un nouveau service",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A1.3.4 Déploiement d'un service",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A1.4.1 Participation à un projet",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A2.2.1 Suivi et résolution d'incidents",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A2.3.1 Identification, qualification et évaluation d'un problème",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.1.1 Proposition d'une solution applicative",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.1.2 Conception ou adaptation de l'interface utilisateur d'une solution",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.1.8 Réalisation des tests nécessaires à la validation d'éléments",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.1.9 Rédaction d'une documentation technique",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.1.1 Rédaction d'une documentation d'utilisation",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.2.1 Analyse et correction d'un dysfonctionnement, d'un problème",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.2.3 Réalisation des tests nécessaires à la mise en production",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A4.2.4 Mise à jour d'une documentation technique",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A5.1.6 Évaluation d'un investissement informatique",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A5.2.3 Repérage des compléments de formation ou d'auto-formation",
+                            rewording: "",
+                            imgUrl: ""
+                        },
+                        {
+                            title: "A5.2.4 Étude d‘une technologie, d'un composant, d'un outi",
+                            rewording: "",
+                            imgUrl: ""
+                        },
                     ]
                 } />
         }
