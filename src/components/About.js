121d304a612e18dcf13d6d37d6a0aeed383f3da3
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2021-02-21 00:32:22 +0100

    Updated about

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Container, Grid, Button } from "@material-ui/core";
 
 import styles from "./About.module.css";
 
 function About(props) {
     return (
-        <Container ref={props.refProp}>
+        <Container ref={props.refProp} className="container">
             <h1 className="title">About me</h1>
             <p className="subTitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            <Row>
-                <Col lg={{ span: 2, offset: 7 }} className={styles.buttonWrapper}>
-                    <Button size="lg" variant="dark"><strong>HIRE ME</strong></Button>
-                </Col>
-                <Col lg={3} className={styles.buttonWrapper}>
-                    <Button size="lg" variant="dark"><strong>DOWNLOAD CV</strong></Button>
-                </Col>
-            </Row>
+            <Grid container spacing={10} className={styles.buttonWrapper}>
+                <Grid item>
+                    <Button variant="contained" size="large"><strong>HIRE ME</strong></Button>
+                </Grid>
+                <Grid item>
+                    <Button variant="contained" size="large"><strong>DOWNLOAD CV</strong></Button>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
