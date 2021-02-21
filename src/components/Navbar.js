7a4afa9ab0389a88ecc62bb9f6b681dcd370b4d5
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2021-02-21 03:36:30 +0100

    Get rid of bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,69 @@
 import React from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { AppBar, Box, IconButton, Menu, MenuItem, Toolbar, Typography, useMediaQuery, useTheme } from "@material-ui/core";
+import { FaBars } from "react-icons/fa";
 
 import styles from "./NavBar.module.css";
 
 function MyNavBar({ sections }) {
-
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleMenu = event => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleButtonsClick = section => {
+        window.scrollTo({ top: sections[section].ref.current.offsetTop - 64 })
+        setAnchorEl(null);
+    };
     const NavBarButtons = () => Object.keys(sections).map(
-        (section, i) => <Nav.Link key={i} onClick={() => window.scrollTo({ behavior: "smooth", top: sections[section].ref.current.offsetTop })} >{section}</Nav.Link>
+        (section, i) => <IconButton color="inherit" key={i} onClick={() => handleButtonsClick(section)} >{section}</IconButton>
     )
 
+    const MobileNavBarButtons = () =>
+        <>
+            <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleMenu}
+            >
+                <FaBars />
+            </IconButton>
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                }}
+                open={open}
+                onClose={() => setAnchorEl(null)}
+            >
+                {Object.keys(sections).map((section,i) =>
+                    <MenuItem key={i} onClick={() => handleButtonsClick(section)}>
+                        {section}
+                    </MenuItem>
+                )}
+            </Menu>
+        </>
+
     return (
-        <Navbar fixed="top" sticky="top" collapseOnSelect expand="sm" bg="dark" variant="dark">
-            <Navbar.Brand className={styles.brand} onClick={() => window.scrollTo({behavior: "smooth", top: 0})}>Antonin J.</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="mr-auto"></Nav>
-                <Nav>
-                    {NavBarButtons()}
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <AppBar color="default" style={{ backgroundColor: "#121212", color: "white" }}>
+            <Toolbar>
+                <Box className={styles.brand}>
+                    <Typography display="inline" variant="h4" className={styles.brandText} onClick={() => window.scrollTo({ top: 0 })}>
+                        Antonin J.
+                    </Typography>
+                </Box>
+                {isMobile ? MobileNavBarButtons() : NavBarButtons()}
+            </Toolbar>
+        </AppBar>
     )
 }
 
