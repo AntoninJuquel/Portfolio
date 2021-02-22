308fe2197d4e6c1bf76961603bbe3644ada61503
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2021-02-22 13:35:28 +0100

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,25 @@
 import React from "react";
-import { Container, Grid } from "@material-ui/core";
+import { Container, Grid, useMediaQuery, useTheme } from "@material-ui/core";
 
 import Hero from "./Hero";
 import ProjectsList from "./ProjectsList";
 
-import styles from "./Projects.module.css";
-
 function Projects(props) {
 
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
     const ProjectsCategory = ({ category }) => ProjectsList[category].map((project, i) =>
-        <Grid item lg={3} md={6} sm={12} key={i} className={styles.grid} >
+        <Grid item lg={3} md={6} sm={12} key={i} style={{textAlign : "center"}}>
             <img src={project.img} width="250" alt={project.title} />
             <Hero subTitle={project.title} center />
         </Grid>
     )
 
     const MyProjects = () => Object.keys(ProjectsList).map((project, i) =>
-        <Container key={i} className={styles.container}>
+        <Container key={i}>
             <Hero title={project} />
-            <Grid container>
+            <Grid style={{ justifyContent: isMobile ? "center" : "normal" }} container>
                 <ProjectsCategory category={project} />
             </Grid>
         </Container>
