59f432bc4ffc13c7756178fa7c9b9e2cbbad5719
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2021-02-27 00:56:52 +0100

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,21 +9,29 @@ function Projects(props) {
 
     const { classes, isMobile } = props
 
-    const ProjectsCategory = ({ category }) => projects[category].map((project, i) =>
-        <Grid item lg={3} md={6} sm={12} key={i} style={{ textAlign: "center" }}>
-            <img src={project.img} width="250" alt={project.title} />
-            <Hero classes={classes} subTitle={project.title} center />
-        </Grid>
-    )
+    function ProjectsCategory({ category }) {
+        return (
+            projects[category].map((project, i) =>
+                <Grid item lg={3} md={6} sm={12} key={i} style={{ textAlign: "center" }}>
+                    <img src={project.img} width="250" alt={project.title} />
+                    <Hero classes={classes} subTitle={project.title} center />
+                </Grid>
+            )
+        )
+    }
 
-    const MyProjects = () => Object.keys(projects).map((project, i) =>
-        <Container key={i}>
-            <Hero classes={classes} title={project} />
-            <Grid style={{ justifyContent: isMobile ? "center" : "normal" }} container>
-                <ProjectsCategory category={project} />
-            </Grid>
-        </Container>
-    )
+    function MyProjects() {
+        return (
+            Object.keys(projects).map((project, i) =>
+                <Container key={i}>
+                    <Hero classes={classes} title={project} />
+                    <Grid style={{ justifyContent: isMobile ? "center" : "normal" }} container>
+                        <ProjectsCategory category={project} />
+                    </Grid>
+                </Container>
+            )
+        )
+    }
 
     return (
         <Container ref={props.refProp} className={classes.container}>
