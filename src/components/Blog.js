0a4930043997de3eb6e44f8b2b2eda9dc4acf70d
Somindras <57025128+AntoninJuquel@users.noreply.github.com>
2021-03-04 00:11:44 +0100

    Added first blog content

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,13 +1,50 @@
-import { Container } from "@material-ui/core";
 import React from "react";
+import { Container, ButtonBase, Grid, Typography } from "@material-ui/core";
 
-import { Hero } from "./CustomComponents";
+import { Carousel, Hero } from "./CustomComponents";
+
+import { useModal } from "../providers/ModalContext";
+import { useLanguage } from "../providers/LanguageContext";
+
+import blogs from "../json/blogs.json";
 
 function Blog(props) {
+    const modal = useModal()
+    const { language} = useLanguage();
     const { classes } = props
+
+    function Blogs() {
+        return (
+            blogs.map((blog, i) =>
+                <Grid item lg={3} md={6} sm={12} key={i} style={{ textAlign: "center" }}>
+                    <ButtonBase>
+                        <img src={blog.img} width="250" alt={blog.title[language]} onClick={() => modal.current.setModal(<BlogModal blog={blog} />)} />
+                    </ButtonBase>
+                    <Hero classes={classes} subTitle={blog.title[language]} center />
+                </Grid>
+            )
+        )
+    }
+
+    function BlogModal({ blog }) {
+        return (
+            <>
+                <Typography>{blog.title[language]}</Typography>
+                <Carousel>
+                    {
+                        blog.carousel[language].map((url, i) =>
+                            <img key={i} src={process.env.PUBLIC_URL + url} alt={i} />)
+                    }
+                </Carousel>
+            </>
+        )
+    }
+
+
     return (
         <Container ref={props.refProp} className={classes.container}>
             <Hero classes={classes} title="Blog" />
+            <Blogs />
         </Container>
     )
 }
